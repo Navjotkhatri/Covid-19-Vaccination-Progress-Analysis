11c7297af8b2596d19699194409737f67f65a891
Navjot_khatri <117521199+Navjotkhatri@users.noreply.github.com>
2023-02-02 11:39:38 +0530

    Add files via upload

--- Covid Vaccination progress Analysis.sql
+++ Covid Vaccination progress Analysis.sql
@@ -3,17 +3,54 @@
 
 select* from vaccinations
 
-
+--total vaccinated and boosters percentage people in india--
 
 select location,population,Max(people_fully_vaccinated) AS total_people_fully_vaccinated
-,(MAX(people_fully_vaccinated)/population)*100 AS vaccinated_population_percentage
+,(MAX(people_fully_vaccinated)/population)*100 AS vaccinated_population_percentage,
+max(total_boosters) AS total_boosters,
+(max(total_boosters)/population)*100 as boosters_dose_percentage 
 from vaccinations
 where location like 'India%'
 group by location,population
 
+---total booster dose percentage in india---
+
+select location,population,max(total_boosters) AS total_boosters,
+(max(total_boosters)/population)*100 as boosters_dose_percentage 
+from vaccinations
+where continent is not null
+and location like 'India%'
+group by location,population
+order by max(total_boosters)
 
 
 
+
+
+--total vaccinated and boosters percentage people in the world--
+
+
+
+select location,population,Max(people_fully_vaccinated) AS total_people_fully_vaccinated
+,(MAX(people_fully_vaccinated)/population)*100 AS vaccinated_population_percentage,
+max(total_boosters) AS total_boosters,
+(max(total_boosters)/population)*100 as boosters_dose_percentage 
+from vaccinations
+where location like 'World%'
+group by location,population
+
+
+----total booster dose percentage in the world
+
+select location,population,max(total_boosters) AS total_boosters,
+(max(total_boosters)/population)*100 as boosters_dose_percentage 
+from vaccinations
+where continent is  null
+and location like 'World%%'
+group by location,population
+order by max(total_boosters)
+
+----total populaion percentage vaccinated in all location--
 
 select location,population,Max(people_fully_vaccinated) AS total_people_fully_vaccinated
 ,(MAX(people_fully_vaccinated)/population)*100 AS vaccinated_population_percentage
@@ -24,7 +61,7 @@ order by vaccinated_population_percentage desc
 
 
 
-
+----vaccination programe progress in india--
 
 select location,date,population,people_fully_vaccinated from vaccinations
 where continent is not null
@@ -32,7 +69,9 @@ and people_fully_vaccinated is not null
 and location like 'India%'
 
 
-select location,population,max(total_boosters) AS total_boosters from vaccinations
+select location,population,max(total_boosters) AS total_boosters,
+(max(total_boosters)/population)*100 as boosters_dose_percentage 
+from vaccinations
 where continent is not null
 and location like 'India%'
 group by location,population
@@ -51,7 +90,8 @@ order by MAX(total_boosters)
 
 
 
-select location,population,max(total_boosters) AS total_boosters 
+select location,population,max(total_boosters) AS total_boosters ,
+(max(total_boosters)/population)*100 AS boosters_dose_percentage
 from vaccinations
 where continent is not null
 --and location like 'India%'
